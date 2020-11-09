2511316906457abba7855a68060b1024852c4472
Ezequiel Girola <ezequielgirola@gmail.com>
2020-11-09 19:40:41 -0300

    Deleted deprecated classes
    - Deleted Deposit,Extract,Transfer
    - Added MovementsController

---
+++ MVCHomeBanking/MVCHomeBanking/Controllers/MovementsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCHomeBanking.Context;
+using MVCHomeBanking.Models;
+
+namespace MVCHomeBanking.Controllers
+{
+    public class MovementsController : Controller
+    {
+        private readonly HomeBankingContext _context;
+
+        public MovementsController(HomeBankingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movements
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.movements.ToListAsync());
+        }
+
+        // GET: Movements/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movement = await _context.movements
+                .FirstOrDefaultAsync(m => m.movementId == id);
+            if (movement == null)
+            {
+                return NotFound();
+            }
+
+            return View(movement);
+        }
+
+        // GET: Movements/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movements/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("movementId,value,type,status,originAccountId,destinationAccountId")] Movement movement)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movement);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movement);
+        }
+
+        // GET: Movements/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movement = await _context.movements.FindAsync(id);
+            if (movement == null)
+            {
+                return NotFound();
+            }
+            return View(movement);
+        }
+
+        // POST: Movements/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("movementId,value,type,status,originAccountId,destinationAccountId")] Movement movement)
+        {
+            if (id != movement.movementId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movement);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovementExists(movement.movementId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movement);
+        }
+
+        // GET: Movements/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movement = await _context.movements
+                .FirstOrDefaultAsync(m => m.movementId == id);
+            if (movement == null)
+            {
+                return NotFound();
+            }
+
+            return View(movement);
+        }
+
+        // POST: Movements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movement = await _context.movements.FindAsync(id);
+            _context.movements.Remove(movement);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovementExists(int id)
+        {
+            return _context.movements.Any(e => e.movementId == id);
+        }
+    }
+}
