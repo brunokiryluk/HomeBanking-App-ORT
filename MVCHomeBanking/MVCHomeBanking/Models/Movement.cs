ef78b49d9c4ed900ecb5df03229c67b4220c2e3c
Bruno Kiryluk <brunokiryluk@gmail.com>
2020-11-16 23:15:02 -0300

    -Add: -Gradients -Custom Table -DisplayName for Movement.cs fields

--- MVCHomeBanking/MVCHomeBanking/Models/Movement.cs
+++ MVCHomeBanking/MVCHomeBanking/Models/Movement.cs
@@ -1,6 +1,7 @@
 ﻿using MVCHomeBanking.Models.MovementsEnums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
@@ -12,12 +13,15 @@ namespace MVCHomeBanking.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int movementId { get; set; }
+        [DisplayName("Monto")]
         public float value { get; set; }
-
+        [DisplayName("Tipo de Operacion")]
         public TYPE_MOVEMENT type { get; set; }
-
+        [DisplayName("Resultado de Movimiento")]
         public STATUS_MOVEMENT status { get; set; }
+        [DisplayName("Cuenta Origen")]
         public string originAccountId { get; set; }
+        [DisplayName("Cuenta Destino")]
         public string destinationAccountId { get; set; }
 
 
