57d5a4c83789d441e663f5156b746166456d8a3b
Ezequiel Girola <ezequielgirola@gmail.com>
2020-11-29 15:01:35 -0300

    QoL Changes
    - Changed  link buttons from scafold to bootstrap
    - Fixed some bug in the KPI

--- MVCHomeBanking/MVCHomeBanking/Models/Client.cs
+++ MVCHomeBanking/MVCHomeBanking/Models/Client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
@@ -10,25 +11,34 @@ namespace MVCHomeBanking.Models
     public class Client
     {
         [Key]
+        [DisplayName("Numero de documento")]
         public String nroDoc { get; set; }
 
         [Required]
         [StringLength(50, MinimumLength = 3)]
+        [DisplayName("Nombre")]
+
         public String name { get; set; }
         [Required]
         [StringLength(50, MinimumLength = 3)]
+        [DisplayName("Apellido")]
+
         public String surname { get; set; }
+        [DisplayName("Telefono")]
         public String phone { get; set; }
 
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
+        [DisplayName("Correo electronico")]
         public string Email { get; set; }
 
         // Credentials of the Client
         [Required]
+        [DisplayName("Nombre de usuario")]
         public String username { get; set; }
 
         [Required]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
+        [DisplayName("Contraseña")]
         public String password { get; set; }
 
         public List<Account> accounts { get; set; }
